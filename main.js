4c96e8fdf0368deafff9562eb5008c806fd551e6
Jihyeon1520 <hyeon_0225@naver.com>
2025-06-02 03:34:15 +0900

    js, css 관련 경로 추가

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ console.log("라우터 타입:", typeof indexRouter);
 // 스타일(CSS) 적용하기
 //static 파일 url로 접근할 수 있도록 
 app.use(express.static(path.join(__dirname, 'src/public')));
+app.use("/partner/css", express.static(path.join(__dirname, "src/public/css")));
+app.use("/partner/js", express.static(path.join(__dirname, "src/public/js")));
 app.use(bodyParser.json());
 //URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 
