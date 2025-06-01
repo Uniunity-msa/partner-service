146e19454bb22d1cc8a965eac5f61b9df5305c70
Jihyeon1520 <hyeon_0225@naver.com>
2025-06-02 01:58:16 +0900

    라우터 변경

--- src/public/js/apiUrl.js
+++ src/public/js/apiUrl.js
@@ -1,8 +1,9 @@
-export const apiUrl = 'http://uniunity';
+export const apiUrl = 'http://uniunity/partner';
 
 // 마이크로서비스별 API 주소
 export const baseUrls = {
-  council: "http://uniunity';",
-  post: "http://uniunity';",
-  user: "http://uniunity';",
+  council: "http://uniunity",
+  post: "http://uniunity",
+  user: "http://uniunity",
+  auth: "http://uniunity"
 };
