434e5aba3e6917866babde63a2a16d49d8451d6b
Jihyeon1520 <hyeon_0225@naver.com>
2025-06-06 03:33:05 +0900

    이동 url 변경

--- src/public/js/partnerUpdate.js
+++ src/public/js/partnerUpdate.js
@@ -195,7 +195,7 @@ storeUploadBtn.addEventListener('click',updateStore);
 // 현재 URL의 경로 일부 가져오기 (partner 뒤의 학교 이름 추출함)
 function getDynamicValueFromURL() {
   var path = window.location.pathname;
-  var regex = /\/partner\/([a-zA-Z]+)/; // /partner/ 다음에 있는 영어 문자열을 추출하는 정규식
+  var regex = /\/partner\/partnerUpdate\/([a-zA-Z]+)/; // /partner/ 다음에 있는 영어 문자열을 추출하는 정규식
   var matches = path.match(regex);
   if (matches && matches.length > 1) {
     return matches[1];
