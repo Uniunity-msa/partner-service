c8e2b737133c50b0a2a3dce24e26b5039f4731f3
Jihyeon1520 <hyeon_0225@naver.com>
2025-06-05 17:02:02 +0900

    db 이름 변경

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,13 @@ const mysql = require('mysql2');
 require("dotenv").config();
 
 const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   connectionLimit: 10,
-  host: process.env.PARTNER_SERVICE_DB_HOST,
-  user: process.env.PARTNER_SERVICE_DB_USER,
-  password: process.env.PARTNER_SERVICE_DB_PASSWORD,
-  database: process.env.PARTNER_SERVICE_DB_DATABASE,
-  port: process.env.PARTNER_SERVICE_DB_PORT,
-  charset: 'utf8mb4'
+  charset: "utf8mb4"
 });
 
 module.exports = { pool };
